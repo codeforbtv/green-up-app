b89accdfa877e04e38bea10f68533bc9ca3bbe57
Nathan Edwards <112483617+NathanWEdwards@users.noreply.github.com>
2026-04-05 21:15:23 -0400

    fix: modal bounds issue pushes the ButtonBar and FlatList out of view when the Modal is not transparent.

--- components/team-members-editor/index.js
+++ components/team-members-editor/index.js
@@ -171,22 +171,24 @@ const TeamMembersEditor = ({ actions, team, members, requests, invitations }: Pr
 
 
     return (
-        <SafeAreaView style={ styles.frame }>
-            <ButtonBar buttonConfigs={ headerButtons }/>
-            <View style={ {
-                flex: 1,
-                backgroundColor: constants.colorBackgroundLight
-            } }>
-                <FlatList data={memberRowData} renderItem={ ({item}) => (<MemberItem item={item} />)}  />
-            </View>
+        <View style={ { flex: 1 } }>
+            <SafeAreaView style={ { backgroundColor: constants.colorBackgroundDark, flex: 1 } }>
+                <ButtonBar buttonConfigs={ headerButtons }/>
+                <View style={ {
+                    flex: 1,
+                    backgroundColor: constants.colorBackgroundLight
+                } }>
+                    <FlatList data={memberRowData} renderItem={ ({item}) => (<MemberItem item={item} />)}  />
+                </View>
+            </SafeAreaView>
             <Modal
                 animationType={ "slide" }
                 onRequestClose={ (): string => ("this function is required. Who knows why?") }
-                transparent={ false }
+                transparent={ true }
                 visible={ isModalVisible }>
                 { modalContent }
             </Modal>
-        </SafeAreaView>
+        </View>
     );
 };
 
